bdf5271fcfb1b265b8414f6080181e0ba8ff3c26
Ante Bucan <ab8390@g.rit.edu>
2024-03-01 11:35:04 +0100

    done

--- 03-task-manager/starter/app.js
+++ 03-task-manager/starter/app.js
@@ -1,1 +1,38 @@
-console.log('Task Manager App')
+require('dotenv').config()
+const express = require('express');
+const connectDB = require('./db/connect')
+const notFound = require('./middleware/not-found');
+const errorHandlerMiddleware = require('./middleware/error-handler');
+
+const app = express()
+
+const tasks = require('./routes/tasks');
+
+// middleware
+app.use(express.static('./public'))
+app.use(express.json())
+
+app.get('/', (req, res) => {
+    res.send('Task Manager App')
+})
+
+app.use('/api/v1/tasks', tasks)
+
+app.use(notFound)
+app.use(errorHandlerMiddleware)
+
+const port = process.env.PORT || 3000;
+
+const start = async () => {
+    try {
+        await connectDB(process.env.MONGO_URI)
+        app.listen(port, () => {
+            console.log(`server is listening on the port ${port}`);
+        })
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+start()
+
