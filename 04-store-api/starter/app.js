4f97984e4818523c67dc91d47e2dec6dcbd2fb51
Ante Bucan <ab8390@g.rit.edu>
2024-03-01 14:44:53 +0100

    sort next

--- 04-store-api/starter/app.js
+++ 04-store-api/starter/app.js
@@ -1,1 +1,40 @@
-console.log('04 Store API')
+require('dotenv').config()
+require('express-async-errors')
+
+const express = require('express')
+
+const app = express()
+
+const notFound = require('./middleware/not-found')
+const errorMiddleware = require('./middleware/error-handler')
+const connectDB = require('./db/connect')
+const productsRouter = require('./routes/products')
+
+// middleware
+app.use(express.json())
+
+// routes
+app.get('/', (req, res) => {
+    res.send('<h1>Store API</h1><a href="/api/v1/products">Products Routes</a>')
+})
+
+app.use('/api/v1/products', productsRouter)
+
+app.use(notFound)
+app.use(errorMiddleware)
+
+const port = process.env.PORT || 3000;
+
+const start = async () => {
+    try {
+        // connect to db
+        await connectDB(process.env.MONGO_URI)
+        app.listen(port, () => {
+            console.log(`server is listening on the port ${port}`);
+        })
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+start()
