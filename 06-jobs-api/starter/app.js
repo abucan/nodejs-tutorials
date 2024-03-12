3c16a649078e6b974831d4971e9374b985eb1980
Ante Bucan <ab8390@g.rit.edu>
2024-03-12 18:52:31 +0100

    finish

--- 06-jobs-api/starter/app.js
+++ 06-jobs-api/starter/app.js
@@ -1,5 +1,12 @@
 require('dotenv').config();
 require('express-async-errors');
+
+// extra
+const helmet = require('helmet');
+const cors = require('cors');
+const xss = require('xss-clean');
+const rateLimit = require('express-rate-limit');
+
 const express = require('express');
 const app = express();
 
@@ -12,15 +19,25 @@ const jobRouter = require('./routes/jobs');
 const notFoundMiddleware = require('./middleware/not-found');
 const errorHandlerMiddleware = require('./middleware/error-handler');
 
+app.set('trust proxy', 1);
+app.use(
+  rateLimit({
+    windowMs: 15 * 60 * 1000,
+    max: 100,
+  }),
+);
 app.use(express.json());
 // extra packages
+app.use(helmet());
+app.use(cors());
+app.use(xss());
 
 // connect db
 const connectDB = require('./db/connect');
 
 // routes
-app.use('/api/v1/auth', authRouter)
-app.use('/api/v1/jobs',auth, jobRouter)
+app.use('/api/v1/auth', authRouter);
+app.use('/api/v1/jobs', auth, jobRouter);
 
 app.use(notFoundMiddleware);
 app.use(errorHandlerMiddleware);
@@ -29,9 +46,9 @@ const port = process.env.PORT || 3000;
 
 const start = async () => {
   try {
-    await connectDB(process.env.MONGO_URI)
+    await connectDB(process.env.MONGO_URI);
     app.listen(port, () =>
-      console.log(`Server is listening on port ${port}...`)
+      console.log(`Server is listening on port ${port}...`),
     );
   } catch (error) {
     console.log(error);
